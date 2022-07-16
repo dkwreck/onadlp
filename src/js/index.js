04c080feb571ae7b165cfa8a2f5b1474c46353d2
Daisuke Katayama <dk@jeksys.com>
2022-07-17 06:38:53 +0900

    G1HTML ver1

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,5 @@
 var utils = require('./utils');
 var axios = require('axios');
-var $ = require('jquery');
 var _u = require('underscore');
 
 var App = {
@@ -10,6 +9,14 @@ var App = {
         // $(window).on('resize', function(){
         //     console.log(utils.matchMedia());
         // });
+
+        $('.slider').slick({
+            prevArrow: '<span class="prev-arrow"><img src="../assets_lp/images/arrow-p.svg" alt="←"></span>',
+            nextArrow: '<span class="next-arrow"><img src="../assets_lp/images/arrow-n.svg" alt="→"></span>',
+            dots: true,
+            centerMode: true,
+            centerPadding: '8.33%',
+        });
     }
 }
 $(function(){
